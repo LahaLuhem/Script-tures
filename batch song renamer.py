182f054d88a82d9825975ea1ef5dd5dd61937efd
Laha Luhem <47954571+LahaLuhem@users.noreply.github.com>
2022-10-06 22:58:48 +0200

    rem redundant

--- batch song renamer.py
+++ batch song renamer.py
@@ -6,16 +6,15 @@ files = [file for file in glob.iglob(input('Enter root directory: ') + '/**/*.mp
 print("\n".join(files), end='\n\n')
 input('%i files. Enter to continue ...' % (len(files)))
 for file in files:
-    abs_filepath = os.path.join(os.path.dirname(__file__), file)
-    containing_folder_path_abs = '\\'.join(abs_filepath.split('\\')[0:-1])
+    containing_folder_path_abs = '\\'.join(file.split('\\')[0:-1])
 
-    [author, name_and_subs] = os.path.basename(abs_filepath).split(' - ', 2)
+    [author, name_and_subs] = os.path.basename(file).split(' - ', 2)
     parts = re.split("\s(?=feat)|\s(?=ft)|\s(?=\()|\s(?=\&)", name_and_subs, 1)
     if len(parts) == 1:
         new_filename = parts[0][0:-4] + ' - ' + author + '.mp3'
     else:
         new_filename = parts[0] + ' - ' + author + ' ' + parts[1]
 
-    os.rename(abs_filepath, containing_folder_path_abs+'\\'+new_filename)
+    os.rename(file, containing_folder_path_abs+'\\'+new_filename)
 
 response = input('... done. Enter to exit...')
