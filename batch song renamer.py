eec6caaa2b91bbfc4d35e22ae26319e6a38cdaaf
Laha Luhem <t3mp0rary13@gmail.com>
2022-10-08 13:46:00 +0200

    added platform support

--- batch song renamer.py
+++ batch song renamer.py
@@ -2,12 +2,14 @@ import glob
 import re
 import os
 
+posix_separator = '\\' if os.uname()[0] == 'Windows' else '/'
+
 files = [file for file in glob.iglob(input('Enter root directory:\n') + '/**/*.mp3', recursive=True)]
 print("\n________________________________________________________________\n\n"+("\n".join(list(map(lambda file: os.path.basename(file), files)))), end='\n')
 print("\n________________________________________________________________\n")
 input('%i files. Enter to continue ...' % (len(files)))
 for file in files:
-    containing_folder_path_abs = '\\'.join(file.split('\\')[0:-1])
+    containing_folder_path_abs = posix_separator.join(file.split(posix_separator)[0:-1])
 
     [author, name_and_subs] = os.path.basename(file).split(' - ', 2)
     parts = re.split("\s(?=feat)|\s(?=ft)|\s(?=\()|\s(?=\&)", name_and_subs, 1)
@@ -16,7 +18,7 @@ for file in files:
     else:
         new_filename = parts[0] + ' - ' + author + ' ' + parts[1]
 
-    os.rename(file, containing_folder_path_abs+'\\'+new_filename)
+    os.rename(file, containing_folder_path_abs+posix_separator+new_filename)
 
 print("\n________________________________________________________________\n")
 
